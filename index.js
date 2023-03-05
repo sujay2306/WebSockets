822f84d86573bbfbaa5ea78e20cf8076e40bf099
sujay2306 <sujayks007@gmail.com>
2023-03-05 18:50:20 +0530

    added websockets

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require("http")
+const WebSocketServer = require("websocket").server
+let connection = null
+
 //spin up a http server
 const httpserver = http.createServer((req, res) => {
     console.log("we have received a request");
 })
 
-httpserver.listen(80, ()=>{
+//create websocket
+const websocket = new WebSocketServer({
+    httpServer:httpserver //we are sending out http server listening on 8080
+})
+websocket.on("request", request=>{
+    //accept the request of client
+   connection = request.accept(null, request.origin)
+   connection.on("onopen", () => console.log("Opened!!!"))
+   connection.on("onclose"), () => console.log("Closed!!!")
+   connection.on("onmessage", message => {
+            console.log(`Rrecived message ${message}`);
+   })
+
+})
+
+httpserver.listen(80, () => {
     console.log("My server is listening on port 8080");
 })
 
